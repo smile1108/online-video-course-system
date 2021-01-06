3c4986cf20348f06b1e9e33b394b82c8110aa5b2
Jia Chang <smile001108@163.com>
2021-01-06 15:12:36 +0800

    init project

---
+++ src/main/java/com/jiac/course/OnlineVideoCourseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jiac.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoCourseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVideoCourseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiac/course/OnlineVideoCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiac.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVideoCourseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
