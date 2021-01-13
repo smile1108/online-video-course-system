02d429134e4316ebbef265fedbefe2e72580ae75
Jia Chang <smile001108@163.com>
2021-01-13 15:05:21 +0800

    1. 增加讲师管理功能

---
+++ business/src/main/java/com/jiac/business/controller/admin/TeacherController.java
@@ -0,0 +1,59 @@
+package com.jiac.business.controller.admin;
+
+import com.jiac.server.domain.Teacher;
+import com.jiac.server.dto.TeacherDto;
+import com.jiac.server.dto.PageDto;
+import com.jiac.server.dto.ResponseDto;
+import com.jiac.server.service.TeacherService;
+import com.jiac.server.util.ValidatorUtil;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * FileName: TeacherController
+ * Author: Jiac
+ * Date: 2021/1/6 16:21
+ */
+@RestController
+@RequestMapping("/admin/teacher")
+public class TeacherController {
+
+    public static final String BUSINESS_NAME = "讲师";
+
+    @Resource
+    private TeacherService teacherService;
+
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto){
+        ResponseDto responseDto = new ResponseDto<>();
+        teacherService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody TeacherDto teacherDto){
+        // 保存校验
+        ValidatorUtil.require(teacherDto.getName(), "姓名");
+        ValidatorUtil.length(teacherDto.getName(), "姓名", 1, 50);
+        ValidatorUtil.length(teacherDto.getNickname(), "昵称", 1, 50);
+        ValidatorUtil.length(teacherDto.getImage(), "头像", 1, 100);
+        ValidatorUtil.length(teacherDto.getPosition(), "职位", 1, 50);
+        ValidatorUtil.length(teacherDto.getMotto(), "座右铭", 1, 50);
+        ValidatorUtil.length(teacherDto.getIntro(), "简介", 1, 500);
+
+        ResponseDto responseDto = new ResponseDto<>();
+        teacherService.save(teacherDto);
+        responseDto.setContent(teacherDto);
+        return responseDto;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id){
+        ResponseDto responseDto = new ResponseDto<>();
+        teacherService.delete(id);
+        return responseDto;
+    }
+}
---
+++ server/src/main/java/com/jiac/server/service/TeacherService.java
@@ -0,0 +1,68 @@
+package com.jiac.server.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiac.server.domain.Teacher;
+import com.jiac.server.domain.TeacherExample;
+import com.jiac.server.dto.TeacherDto;
+import com.jiac.server.dto.PageDto;
+import com.jiac.server.mapper.TeacherMapper;
+import com.jiac.server.util.CopyUtil;
+import com.jiac.server.util.UuidUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FileName: TeacherService
+ * Author: Jiac
+ * Date: 2021/1/7 10:22
+ */
+@Service
+public class TeacherService {
+
+    @Resource
+    private TeacherMapper teacherMapper;
+
+    public void list(PageDto pageDto){
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        TeacherExample teacherExample = new TeacherExample();
+        List<Teacher> teacherList = teacherMapper.selectByExample(teacherExample);
+        PageInfo<Teacher> pageInfo = new PageInfo<>(teacherList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<TeacherDto> teacherDtoList = new ArrayList<>();
+        for(int i = 0, l = teacherList.size(); i < l; i++){
+            Teacher teacher = teacherList.get(i);
+            TeacherDto teacherDto = new TeacherDto();
+            BeanUtils.copyProperties(teacher, teacherDto);
+            teacherDtoList.add(teacherDto);
+        }
+        pageDto.setList(teacherDtoList);
+    }
+
+    public void save(TeacherDto teacherDto){
+        Teacher teacher = CopyUtil.copy(teacherDto, Teacher.class);
+        if(StringUtils.isEmpty(teacherDto.getId())){
+            this.insert(teacher);
+        } else {
+            this.update(teacher);
+        }
+    }
+
+    private void insert(Teacher teacher){
+        teacher.setId(UuidUtil.getShortUuid());
+        teacherMapper.insert(teacher);
+    }
+
+    private void update(Teacher teacher){
+        teacherMapper.updateByPrimaryKey(teacher);
+    }
+
+    public void delete(String id) {
+        teacherMapper.deleteByPrimaryKey(id);
+    }
+}
