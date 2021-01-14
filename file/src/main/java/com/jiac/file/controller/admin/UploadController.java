abb067c95e7f5c731bc66cbd1067aebd00d20847
Jia Chang <smile001108@163.com>
2021-01-14 09:39:37 +0800

    1. 文件上传功能开发: SpringBoot基本的文件上传功能

---
+++ file/src/main/java/com/jiac/file/controller/admin/UploadController.java
@@ -0,0 +1,49 @@
+package com.jiac.file.controller.admin;
+
+import com.jiac.server.domain.Test;
+import com.jiac.server.dto.ResponseDto;
+import com.jiac.server.service.TestService;
+import com.jiac.server.util.UuidUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * FileName: TestController
+ * Author: Jiac
+ * Date: 2021/1/6 16:21
+ */
+@RestController("/admin")
+public class UploadController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
+
+
+
+    @PostMapping("/upload")
+    public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
+        LOG.info("上传文件开始：{}", file);
+        LOG.info(file.getOriginalFilename());
+        LOG.info(String.valueOf(file.getSize()));
+
+        // 保存文件到本地
+        String fileName = file.getOriginalFilename();
+        String key = UuidUtil.getShortUuid();
+        String fullPath = "C:/Users/jc/Desktop/github/online_video_course_system/" + key + "-" + fileName;
+        File dest = new File(fullPath);
+        file.transferTo(dest);
+        LOG.info(dest.getAbsolutePath());
+
+        ResponseDto responseDto = new ResponseDto();
+        return responseDto;
+    }
+}
