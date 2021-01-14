231eae105011d013115064ad42b29fe25f374e0f
Jia Chang <smile001108@163.com>
2021-01-14 12:24:31 +0800

    1. 文件上传功能开发：文件路径配置优化

--- file/src/main/java/com/jiac/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/jiac/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.jiac.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,10 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     // 静态资源的配置
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/f/**")
-                .addResourceLocations("file:C:/Users/jc/Desktop/github/online_video_course_system/");
+                .addResourceLocations("file:" + FILE_PATH);
     }
 }
