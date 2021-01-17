48240da9eb93a9a043ff1ce79e3295f824f7c5d2
Jia Chang <smile001108@163.com>
2021-01-17 09:33:20 +0800

    1. 用户管理与登录：登录增加验证码校验 2. 每次ajax请求，对应session不一致 解决方法(应该不止这一种) 前端配置中增加axios.defaults.withCredentials=true;

--- server/src/main/java/com/jiac/server/dto/UserAdminDto.java
+++ server/src/main/java/com/jiac/server/dto/UserAdminDto.java
@@ -2,62 +2,91 @@ package com.jiac.server.dto;
 
 
 public class UserAdminDto {
-        /**
-         * id
-         */
-        private String id;
-        /**
-         * 登录名
-         */
-        private String loginName;
-        /**
-         * 昵称
-         */
-        private String name;
-        /**
-         * 密码
-         */
-        private String password;
-        public String getId(){
-            return id;
-        }
+    /**
+     * id
+     */
+    private String id;
+    /**
+     * 登录名
+     */
+    private String loginName;
+    /**
+     * 昵称
+     */
+    private String name;
+    /**
+     * 密码
+     */
+    private String password;
 
-        public void setId(String id){
-            this.id = id;
-        }
-        public String getLoginName(){
-            return loginName;
-        }
+    /**
+     * 验证码
+     */
+    private String imageCode;
 
-        public void setLoginName(String loginName){
-            this.loginName = loginName;
-        }
-        public String getName(){
-            return name;
-        }
+    /**
+     * 图片验证码token
+     */
+    private String imageCodeToken;
 
-        public void setName(String name){
-            this.name = name;
-        }
-        public String getPassword(){
-            return password;
-        }
+    public String getId() {
+        return id;
+    }
 
-        public void setPassword(String password){
-            this.password = password;
-        }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImageCode() {
+        return imageCode;
+    }
+
+    public void setImageCode(String imageCode) {
+        this.imageCode = imageCode;
+    }
+
+    public String getImageCodeToken() {
+        return imageCodeToken;
+    }
+
+    public void setImageCodeToken(String imageCodeToken) {
+        this.imageCodeToken = imageCodeToken;
+    }
 
     @Override
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-            sb.append(", id=").append(id);
-            sb.append(", loginName=").append(loginName);
-            sb.append(", name=").append(name);
-            sb.append(", password=").append(password);
-        sb.append("]");
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("UserAdminDto{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", loginName='").append(loginName).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", imageCode='").append(imageCode).append('\'');
+        sb.append(", imageCodeToken='").append(imageCodeToken).append('\'');
+        sb.append('}');
         return sb.toString();
     }
 }
--- system/src/main/java/com/jiac/system/controller/admin/UserAdminController.java
+++ system/src/main/java/com/jiac/system/controller/admin/UserAdminController.java
@@ -4,7 +4,10 @@ import com.jiac.server.domain.UserAdmin;
 import com.jiac.server.dto.*;
 import com.jiac.server.service.UserAdminService;
 import com.jiac.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -21,6 +24,8 @@ import java.util.List;
 public class UserAdminController {
 
     public static final String BUSINESS_NAME = "用户";
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserAdminController.class);
 
     @Resource
     private UserAdminService userAdminService;
@@ -75,9 +80,28 @@ public class UserAdminController {
      */
     @PostMapping("/login")
     public ResponseDto login(@RequestBody UserAdminDto userAdminDto, HttpServletRequest request){
+        LOG.info("用户登录开始");
         userAdminDto.setPassword(DigestUtils.md5DigestAsHex(userAdminDto.getPassword().getBytes()));
 
         ResponseDto responseDto = new ResponseDto<>();
+
+        // 根据验证码token去获取缓存中的验证码，和用户输入的验证码是否一致
+        String imageCode = (String) request.getSession().getAttribute(userAdminDto.getImageCode());
+        if(StringUtils.isEmpty(imageCode)) {
+            responseDto.setSuccess(false);
+            responseDto.setMessage("验证码已过期");
+            LOG.info("用户登录失败");
+            return responseDto;
+        }
+        if(!imageCode.toLowerCase().equals(userAdminDto.getImageCode().toLowerCase())) {
+            responseDto.setSuccess(false);
+            responseDto.setMessage("验证码不正确");
+            LOG.info("用户登录失败");
+            return responseDto;
+        } else {
+            // 验证通过后，移除验证码
+            request.getSession().removeAttribute(userAdminDto.getImageCodeToken());
+        }
         LoginUserAdminDto loginUserAdminDto = userAdminService.login(userAdminDto);
         request.getSession().setAttribute(Constants.LOGIN_USER, loginUserAdminDto);
         responseDto.setContent(loginUserAdminDto);
