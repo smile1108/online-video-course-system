8f8186a5750904b03e7fac6cfa8946ed82e505c2
Jia Chang <smile001108@163.com>
2021-01-19 09:37:43 +0800

    1. 网站接口开发：网站分类接口的开发

---
+++ business/src/main/java/com/jiac/business/controller/web/CategoryController.java
@@ -0,0 +1,65 @@
+package com.jiac.business.controller.web;
+
+import com.jiac.server.dto.CategoryDto;
+import com.jiac.server.dto.PageDto;
+import com.jiac.server.dto.ResponseDto;
+import com.jiac.server.service.CategoryService;
+import com.jiac.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * FileName: CategoryController
+ * Author: Jiac
+ * Date: 2021/1/6 16:21
+ */
+@RestController("webCategoryController")
+@RequestMapping("/web/category")
+public class CategoryController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+    public static final String BUSINESS_NAME = "分类";
+
+    @Resource
+    private CategoryService categoryService;
+
+    @PostMapping("/all")
+    public ResponseDto all(){
+        ResponseDto responseDto = new ResponseDto<>();
+        List<CategoryDto> categoryDtoList = categoryService.all();
+        responseDto.setContent(categoryDtoList);
+        return responseDto;
+    }
+
+//    @PostMapping("/list")
+//    public ResponseDto list(@RequestBody PageDto pageDto){
+//        ResponseDto responseDto = new ResponseDto<>();
+//        categoryService.list(pageDto);
+//        responseDto.setContent(pageDto);
+//        return responseDto;
+//    }
+//
+//    @PostMapping("/save")
+//    public ResponseDto save(@RequestBody CategoryDto categoryDto){
+//        // 保存校验
+//        ValidatorUtil.require(categoryDto.getParent(), "父id");
+//        ValidatorUtil.require(categoryDto.getName(), "名称");
+//        ValidatorUtil.length(categoryDto.getName(), "名称", 1, 50);
+//
+//        ResponseDto responseDto = new ResponseDto<>();
+//        categoryService.save(categoryDto);
+//        responseDto.setContent(categoryDto);
+//        return responseDto;
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public ResponseDto delete(@PathVariable String id){
+//        ResponseDto responseDto = new ResponseDto<>();
+//        categoryService.delete(id);
+//        return responseDto;
+//    }
+}
